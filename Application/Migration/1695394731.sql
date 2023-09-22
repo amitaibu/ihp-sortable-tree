5bbe74089048ac5a7956ce6abd82c6fbc34a9f75
Amitai Burstein <amitai@gizra.com>
2023-09-22 17:59:10 +0300

    Generate

--- Application/Migration/1695394731.sql
+++ Application/Migration/1695394731.sql
@@ -1,7 +1,6 @@
--- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
 CREATE TABLE tasks (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     body TEXT NOT NULL,
-    tasks_id UUID DEFAULT NULL,
+    tasks_id UUID DEFAULT null,
     weight INT DEFAULT 0 NOT NULL
 );
