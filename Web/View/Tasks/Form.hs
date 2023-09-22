deef3648d9c9ad2a3f3437f01a8c8021441b8c12
Amitai Burstein <amitai@gizra.com>
2023-09-23 00:23:01 +0300

    Add delete

--- Web/View/Tasks/Form.hs
+++ Web/View/Tasks/Form.hs
@@ -1,13 +1,23 @@
 module Web.View.Tasks.Form where
 
 import Web.View.Prelude
+import Text.Blaze.Html4.FrameSet.Attributes (href)
 
 renderForm :: Task -> [Task] ->  Html
 renderForm task children = formFor task [hsx|
         {(hiddenField #taskId)}
         <div class="flex flex-col gap-4">
             {(textareaField #body) {fieldLabel = ""}}
-            {submitButton {label = "Save"}}
+
+            <div class="flex flex-row justify-between items-end">
+                {submitButton {label = "Save"}}
+                {deleteLink}
+            </div>
         </div>
     |]
+        where deleteLink
+                -- Show delete link only if there are no children.
+                | isEmpty children = [hsx|<a href={DeleteTaskAction task.id} class="js-delete text-sm hover:underline">Delete</a>|]
+                | otherwise = ""
+
 
